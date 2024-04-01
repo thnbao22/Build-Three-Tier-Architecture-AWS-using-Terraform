e236b9257a709230bdc4dfa287e9edc28c8a9253
Trương Trần Thiên Bảo <baopk22092004@gmail.com>
2024-04-01 09:41:20 +0700

    Define launch Template and auto scaling group for backend

--- compute/main.tf
+++ compute/main.tf
@@ -35,6 +35,7 @@ resource "aws_autoscaling_group" "three_tier_bastion" {
   max_size = 1
   desired_capacity = 1
   vpc_zone_identifier = var.public_subnets
+
   launch_template {
     id = aws_launch_template.three_tier_bastion.id
     version = "$Latest"
@@ -44,11 +45,11 @@ resource "aws_autoscaling_group" "three_tier_bastion" {
 
 
 
-# Define Launch template for app Auto Scaling Group
-resource "aws_launch_template" "three_tier_app" {
+# Define Launch template for FE app Auto Scaling Group
+resource "aws_launch_template" "three_tier_frontend_app" {
   
   # name_prefix: (Optional) Creates a unique name beginning with the specified prefix.
-  name_prefix = "three_tier_app"
+  name_prefix = "three_tier_frontend_app"
   
   # In your launch template you need to specify an instance type. Example: t2.micro
   instance_type = var.instance_type
@@ -68,7 +69,7 @@ resource "aws_launch_template" "three_tier_app" {
   user_data = filebase64("install_apache.sh")
 
   tags = {
-    Name = "three_tier_app" 
+    Name = "three_tier_frontend_app" 
   }
 }                 
 
@@ -78,18 +79,59 @@ data "aws_alb_target_group" "three_tier_tg" {
   
 }
 
-# Define Auto Scaling Group for app 
-resource "aws_autoscaling_group" "three_tier_app" {
-  name = "three_tier_app"
+# Define Auto Scaling Group for FE app
+resource "aws_autoscaling_group" "three_tier_frontend_app" {
+  name = "three_tier_frontend_app"
   min_size = 2
   max_size = 3
   desired_capacity = 3
   vpc_zone_identifier = var.public_subnets
-  
-  target_group_arns = [data.aws_alb_target_group.three_tier_app.arn]
+
+
+  target_group_arns = [data.aws_alb_target_group.three_tier_frontend_app.arn]
   
   launch_template {
-    id = aws_launch_template.three_tier_bastion.id
+    id = aws_launch_template.three_tier_frontend_app.id
+    version = "$Latest"
+  } 
+}
+
+resource "aws_launch_template" "three_tier_backend_app" {
+  
+  # name_prefix: (Optional) Creates a unique name beginning with the specified prefix.
+  name_prefix = "three_tier_backend_app"
+  
+  # In your launch template you need to specify an instance type. Example: t2.micro
+  instance_type = var.instance_type
+
+  # The Image Id you want to launch the instance. Example: Amazon Linux 2023 AMI, ...
+  image_id = data.aws_ssm_parameter.three_tier_ami.value
+  
+  vpc_security_group_ids = [var.backend_app_sg]
+
+  # The key name to use for the instance
+  key_name = var.key_name
+
+  user_data = filebase64("install_node.sh")
+
+  tags = {
+    Name = "three_tier_backend_app" 
+  }
+}             
+
+
+resource "aws_autoscaling_group" "three_tier_backend_app" {
+  name = "three_tier_backend_app"
+  min_size = 2
+  max_size = 3
+  desired_capacity = 2
+  vpc_zone_identifier = var.private_subnets
+
+
+  target_group_arns = [data.aws_alb_target_group.three_tier_backend_app.arn]
+  
+  launch_template {
+    id = aws_launch_template.three_tier_backend_app.id
     version = "$Latest"
   } 
 }
