8d11e70365f15448579c9d304218d5209de5fca3
Trương Trần Thiên Bảo <baopk22092004@gmail.com>
2024-04-03 13:26:30 +0700

    Add file backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,17 @@
 terraform {
   backend "s3" {
-    bucket = "mybucket"
+    bucket = "charlesbucketlab"
     key    = "path/to/my/key"
     # Region: Singapore
     region = "ap-southeast-1"
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source = "harshicop/aws"
+      version = "~> 5.11"
+    }
   }
 }
