7498c6e5e2733b8242cb991d30e89f9fd90c0e27
Trương Trần Thiên Bảo <baopk22092004@gmail.com>
2024-04-04 21:56:49 +0700

    Add explanation about output of the module load balancing

--- modules/loadbalancing/outputs.tf
+++ modules/loadbalancing/outputs.tf
@@ -3,15 +3,15 @@ output "alb_dns" {
   value = aws_lb.three_tier_lb.dns_name
 }
 
-
+# Output the endpoint of the lb
 output "lb_endpoint" {
   value = aws_lb.three_tier_lb.dns_name
 }
 
+# Output the name of the lb
 output "lb_tg_name" {
   value = aws_lb_target_group.three_tier_tg.name
 }
-
 
 output "lb_tg" {
   value = aws_lb_target_group.three_tier_tg.arn
