d96ff492c37524857ee4f87648880d3e4a5cfcb3
Trương Trần Thiên Bảo <baopk22092004@gmail.com>
2024-04-04 21:55:38 +0700

    Add explantion on the output of the modules compute

--- modules/compute/outputs.tf
+++ modules/compute/outputs.tf
@@ -1,7 +1,9 @@
+# Output the frontend auto scaling group
 output "app_asg" {
   value = aws_autoscaling_group.three_tier_frontend_app
 }
 
+# Output the backend auto scaling group
 output "app_backend_asg" {
   value = aws_autoscaling_group.three_tier_backend_app
 }
