12c58b897f020a2a0823596afe63e6fcfb4b52d8
Trương Trần Thiên Bảo <baopk22092004@gmail.com>
2024-04-05 22:07:16 +0700

    Fix the syntax error

--- terraform/install_node.sh
+++ terraform/install_node.sh
@@ -2,5 +2,5 @@
 # Script to install node
 yum update -y
 yum install -y gcc-c++ make
-curl sl https://rpm.nodesource.com/setup_16.x | bash -
+curl -sl https://rpm.nodesource.com/setup_16.x | bash -
 yum install -y nodejs
